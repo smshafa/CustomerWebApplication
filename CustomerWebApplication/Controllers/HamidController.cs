1310e15d8e3435883784499485bacc5cf9a0a7e9
accer <accer@ACCER-DESKTOP>
2019-07-23 14:19:57 +0430

    Show only Province in a Combobox.

--- CustomerWebApplication/Controllers/HamidController.cs
+++ CustomerWebApplication/Controllers/HamidController.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Customer.ServiceLayer.ViewModels;
 
 namespace CustomerWebApplication.Controllers
 {
     public class HamidController : Controller
     {
-        // GET: Hamid
-        public ActionResult Index()
+        [HttpGet]
+        // GET: NewCustomer
+        public ActionResult CusForm()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CusForm(CustomerViewModel customerViewModel)
+        {
+            if (ModelState.IsValid == true)
+            {
+                string name = customerViewModel.FirstName;
+            }
+
             return View();
         }
     }
--- CustomerWebApplication/Controllers/NewCustomerController.cs
+++ CustomerWebApplication/Controllers/NewCustomerController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
+using Customer.BusinessLayer;
 using Customer.ServiceLayer.ViewModels;
 
 namespace CustomerWebApplication.Controllers
@@ -13,7 +15,33 @@ namespace CustomerWebApplication.Controllers
         // GET: NewCustomer
         public ActionResult CusForm()
         {
+
+            ViewBag.CityList = ToSelectList();
             return View();
+        }
+
+
+        //https://www.c-sharpcorner.com/article/dropdownlist-in-asp-net-mvc/
+        [NonAction]
+        public SelectList ToSelectList()
+        {
+            List<SelectListItem> list = new List<SelectListItem>();
+
+            CustomerUnitOfWork unitOfWork = new CustomerUnitOfWork();
+            var provinces = unitOfWork.GetRepoInstance<Customer.DataLayer.Province>().GetAll();
+            var q = from c in provinces
+                    select new { c.ProvinceID, c.ProvinceName };
+
+            foreach(var p in q)
+            {
+                list.Add(new SelectListItem()
+                {
+                    Text = p.ProvinceName,
+                    Value = p.ProvinceID.ToString()
+                });
+            }
+
+            return new SelectList(list, "Value", "Text");
         }
 
         [HttpPost]
@@ -25,6 +53,25 @@ namespace CustomerWebApplication.Controllers
             }
 
             return View();
+        }
+
+        public ActionResult SelectCategory()
+        {
+
+            List<SelectListItem> items = new List<SelectListItem>();
+
+            items.Add(new SelectListItem { Text = "Action", Value = "0" });
+
+            items.Add(new SelectListItem { Text = "Drama", Value = "1" });
+
+            items.Add(new SelectListItem { Text = "Comedy", Value = "2", Selected = true });
+
+            items.Add(new SelectListItem { Text = "Science Fiction", Value = "3" });
+
+            ViewBag.MovieType = items;
+
+            return View();
+
         }
 
 

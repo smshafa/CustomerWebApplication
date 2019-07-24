6623972664f6377b0222c281b5c2540564c86c35
accer <accer@ACCER-DESKTOP>
2019-07-24 16:33:44 +0430

    mapping of UntiTest  project is fixed.

--- UnitTestCustomerProject/MappingProfile.cs
+++ UnitTestCustomerProject/MappingProfile.cs
@@ -29,6 +29,24 @@ namespace UnitTestCustomerProject
 
 
 
+            //CreateMap<Customer.DataLayer.Customer, CustomerViewModel>()
+            //    .ForMember(d => d.FirstName,
+            //        opt => opt.MapFrom(src => src.FirstName)
+            //    )
+            //    .ForMember(d => d.LastName,
+            //        opt => opt.MapFrom(src => src.LastName)
+            //    )
+            //    .ForMember(d => d.ProvinceName,
+            //        opt => opt.MapFrom(src => src.City.Province.ProvinceName)
+            //    )
+            //    .ForMember(d => d.CityName,
+            //        opt => opt.MapFrom(src => src.City.CityName)
+            //    ).ReverseMap();
+
+            //We can map both directions with ReverseMap() method, including unflattening:
+
+
+
             CreateMap<Customer.DataLayer.Customer, CustomerViewModel>()
                 .ForMember(d => d.FirstName,
                     opt => opt.MapFrom(src => src.FirstName)
@@ -41,9 +59,12 @@ namespace UnitTestCustomerProject
                 )
                 .ForMember(d => d.CityName,
                     opt => opt.MapFrom(src => src.City.CityName)
+                )
+                .ForMember(d => d.CityList,
+                    opt => opt.Ignore()).ReverseMap()   // Accer is added this code. this extra list for dropDownList
+                .ForMember(d => d.CustomerID,
+                    opt => opt.Ignore()         // Accer is added this code. this extra list for gridView
                 ).ReverseMap();
-
-            //We can map both directions with ReverseMap() method, including unflattening:
 
 
             //CreateMap<Customer.DataLayer.Customer, CustomerViewModel>()

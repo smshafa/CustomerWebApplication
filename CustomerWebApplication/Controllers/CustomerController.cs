e5b4ed2b2faeabf20f494df8f7e396f6ceea91eb
smshafa <s.m.shafaei@outlook.com>
2019-07-25 09:09:51 +0430

    Delete operatoin is added.

--- CustomerWebApplication/Controllers/CustomerController.cs
+++ CustomerWebApplication/Controllers/CustomerController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web;
 using System.Web.Configuration;
 using System.Web.Mvc;
@@ -12,14 +15,20 @@ namespace CustomerWebApplication.Controllers
 {
     public class CustomerController : Controller
     {
-        // GET: Customer
-        public ActionResult Index()
+        private IEnumerable<CustomerViewModel> getCustomerViewModel()
         {
             CustomerUnitOfWork unitOfWork = new CustomerUnitOfWork();
             IEnumerable<Customer.DataLayer.Customer> customers = unitOfWork.GetRepoInstance<Customer.DataLayer.Customer>().GetAll();
 
             // Mapping 
             var result = Mapper.Map<IEnumerable<Customer.DataLayer.Customer>, IEnumerable<CustomerViewModel>>(customers);
+            return result;
+        }
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var result = getCustomerViewModel();
 
             return View(result);
         }
@@ -28,6 +37,25 @@ namespace CustomerWebApplication.Controllers
         {
 
             return View();
+        }
+
+        public ViewResult Delete(int? id)
+        {
+            if (id == null)
+                return View();
+            HttpClient client = new HttpClient();
+
+            client.BaseAddress = new Uri("https://localhost:44311/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            // New code:
+            HttpResponseMessage response = client.DeleteAsync($"api/customers/{id}").Result;
+
+            if (response.StatusCode != HttpStatusCode.OK)
+                Console.WriteLine("deleted");
+
+            return View("Index", getCustomerViewModel());
         }
     }
 }
